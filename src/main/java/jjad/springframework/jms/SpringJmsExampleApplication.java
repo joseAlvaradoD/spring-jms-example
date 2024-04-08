a5ba451cc2bc0bfcedd862056c685c43872582d2
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-08 15:52:54 -0600

    init commit

---
+++ src/main/java/jjad/springframework/jms/SpringJmsExampleApplication.java
@@ -0,0 +1,13 @@
+package jjad.springframework.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJmsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjad/springframework/jms/SpringJmsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jjad.springframework.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
